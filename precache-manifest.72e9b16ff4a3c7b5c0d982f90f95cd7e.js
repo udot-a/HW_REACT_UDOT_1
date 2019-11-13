a999faec80fb6e0ee3276dacc687ef14ce252504
Udot Andrii <udot.dron@gmail.com>
2019-11-13 14:18:53 +0200

    Updates

---
+++ precache-manifest.72e9b16ff4a3c7b5c0d982f90f95cd7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c177aaee5c827a95b838064de0ca2e74",
+    "url": "/HW_REACT_UDOT_1/index.html"
+  },
+  {
+    "revision": "b21f9b3d0a6bfee6a6cc",
+    "url": "/HW_REACT_UDOT_1/static/css/main.d46570b9.chunk.css"
+  },
+  {
+    "revision": "ea06d7d556fd87ee386a",
+    "url": "/HW_REACT_UDOT_1/static/js/2.d0bf2781.chunk.js"
+  },
+  {
+    "revision": "b21f9b3d0a6bfee6a6cc",
+    "url": "/HW_REACT_UDOT_1/static/js/main.08a4b6b5.chunk.js"
+  },
+  {
+    "revision": "8d55e262bb149488cb89",
+    "url": "/HW_REACT_UDOT_1/static/js/runtime-main.b8056d1b.js"
+  },
+  {
+    "revision": "e45d9d1f63b5b15b9ebf933e7d9088d3",
+    "url": "/HW_REACT_UDOT_1/static/media/252.e45d9d1f.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/HW_REACT_UDOT_1/static/media/logo.25bf045c.svg"
+  }
+]);
